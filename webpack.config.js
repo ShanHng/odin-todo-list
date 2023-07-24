6b0889660ef06684afc979624d8b088f1b1629df
ShanHng <hngshanhern@gmail.com>
2023-07-24 17:50:17 +0800

    Update entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    model: './src/model.js',
+    todoCard: './src/todoCard.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
