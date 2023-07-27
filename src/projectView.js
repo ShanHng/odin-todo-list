9f5c171493fe1a139b1ca8df086b657c35fbbcf7
ShanHng <hngshanhern@gmail.com>
2023-07-27 20:51:01 +0800

    Add New Todo Button

--- src/projectView.js
+++ src/projectView.js
@@ -4,11 +4,13 @@ const projectViewController = (() => {
   const PLACEHOLDER_MESSAGE_EMPTY = 'No todos to see here'
   const PLACEHOLDER_MESSAGE_PROMPT_ACTION =
     'Click on a project to view its todos!'
+  const FA_PLUS_CIRCLE = '<i class="fa-solid fa-plus-circle"></i>'
 
   const placeholder = document.createElement('div')
   const container = document.createElement('div')
   const titleDisplay = document.createElement('div')
   const todoCardContainer = document.createElement('div')
+  const addTodoButton = document.createElement('button')
 
   let todoCards = []
 
@@ -16,8 +18,10 @@ const projectViewController = (() => {
   placeholder.className = 'proj-view-placeholder visible'
   titleDisplay.className = 'proj-view-title'
   todoCardContainer.className = 'proj-view-card-container'
+  addTodoButton.className = 'proj-view-add-todo-btn'
 
   placeholder.textContent = PLACEHOLDER_MESSAGE_PROMPT_ACTION
+  addTodoButton.innerHTML = FA_PLUS_CIRCLE;
 
   function displayPlaceholder (isVisible) {
     if (isVisible) {
@@ -28,7 +32,7 @@ const projectViewController = (() => {
     todoCardContainer.classList.toggle('hidden', isVisible)
   }
 
-  container.append(placeholder, titleDisplay, todoCardContainer)
+  container.append(placeholder, titleDisplay, todoCardContainer, addTodoButton)
 
   return {
     getProjectView () {
@@ -51,7 +55,7 @@ const projectViewController = (() => {
         todoCardContainer.append(todoCard)
       }
     },
-    removeChildFromTodoCardContainer(childNode) {
+    removeChildFromTodoCardContainer (childNode) {
       todoCardContainer.removeChild(childNode)
     }
   }
