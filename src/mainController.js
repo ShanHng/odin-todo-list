c6b63bd12bb85784132486b0972652a62b4285c2
ShanHng <hngshanhern@gmail.com>
2023-08-03 19:42:49 +0800

    Add functions to save and load data
    Existing functions that modifies the projectCatalogue will now save data upon each call so data is immediately saved whenever user makes an action.
    Loading data happens only when data has been saved before.

--- src/mainController.js
+++ src/mainController.js
@@ -2,19 +2,16 @@ import projectCatalogue from './model'
 import newTodoModal from './newTodoModal'
 import projectMenuController from './projectMenu'
 import projectViewController from './projectView'
+import storage from './storage'
 
 const mainController = (() => {
   function createNewProject (projTitle) {
-    projectCatalogue.addProject(projTitle)
+    projectCatalogue.addProject(projTitle, false)
+    saveState()
   }
 
   function getProject (projTitle) {
     return projectCatalogue.getReadOnlyProject(projTitle)
-  }
-
-  function initialize () {
-    const projects = projectCatalogue.getAllReadOnlyProject()
-    projectMenuController.loadMenuItems(projects)
   }
 
   function run () {
@@ -25,25 +22,44 @@ const mainController = (() => {
       projectViewController.getProjectView(),
       newTodoModal
     )
-    projectCatalogue.setUpDefault()
-    initialize()
+    loadState()
+    const projects = projectCatalogue.getAllReadOnlyProject()
+    projectMenuController.loadMenuItems(projects)
   }
 
   function deleteProject (projTitle) {
     projectCatalogue.deleteProject(projTitle)
+    saveState()
   }
 
   function deleteTodoFromProject (todoTitle, projTitle) {
     projectCatalogue.deleteTodoFromProject(todoTitle, projTitle)
+    saveState()
   }
 
   function setTodoAsDone (boolean, todoTitle, projTitle) {
     projectCatalogue.setTodoAsDone(boolean, todoTitle, projTitle)
+    saveState()
   }
 
   function addNewTodoToProject (title, desc, dueDate, priority, projTitle) {
     projectCatalogue.addTodoToProject(title, desc, dueDate, priority, projTitle)
+    saveState()
   }
+
+  function saveState () {
+    console.log(JSON.stringify(projectCatalogue))
+    storage.saveData(projectCatalogue)
+  }
+
+  function loadState () {
+    if (!storage.isPageVisited()) {
+      projectCatalogue.setUpDefault()
+    } else {
+      projectCatalogue.load(storage.getData())
+    }
+  }
+
   return {
     run,
     createNewProject,
