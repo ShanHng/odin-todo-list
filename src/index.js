dab77578a4ba721fdd6fa3bae2b5e89e54abbecf
ShanHng <hngshanhern@gmail.com>
2023-10-02 18:02:21 +0800

    Replace script tags to import fontawesome with import statements

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css'
 import mainController from './mainController'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
 
 mainController.run()
